d1f6390bc43df249a26a49970ad94d70846fe6cf
mritunjayRoy <mroy8202@gmail.com>
2026-04-25 13:45:22 +0530

    initialize Spring Boot project with basic dependencies

---
+++ src/main/java/com/mritunjay/zomato/oms/ZomatoOmsApplication.java
@@ -0,0 +1,13 @@
+package com.mritunjay.zomato.oms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZomatoOmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZomatoOmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mritunjay/zomato/oms/ZomatoOmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mritunjay.zomato.oms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZomatoOmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
